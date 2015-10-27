789afbbec20d3b2d0e292e3a28f43f4711b69bf4
Xiaoyi Chen <cxychina@gmail.com>
2015-10-26 21:48:47 -0700

    tests

--- karma.conf.js
+++ karma.conf.js
@@ -50,6 +50,7 @@ module.exports = function(config) {
     plugins: [
       'karma-jasmine',
       'karma-phantomjs-launcher',
+      'karma-firefox-launcher',
       require("karma-webpack")
     ],
 
@@ -79,7 +80,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', 'Firefox'],
 
 
     // Continuous Integration mode
--- test/objectDescription-test.js
+++ test/objectDescription-test.js
@@ -1,12 +1,100 @@
 import React, {addons, PropTypes} from 'react/addons';
 
 const TestUtils = addons.TestUtils;
+const ObjectDescription = require('../src/ObjectDescription');
 
 describe('ObjectDescription', () => {
 
-  it('hello', function(){
-    // just a placeholder for now
-    expect(2).toEqual(2);
+  beforeEach(() => {
+
   });
+
+  it('number', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={0} />);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("0");
+  });
+
+  it('string', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={"octocat"} />);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("\"octocat\"");
+  });
+
+  it('boolean', () => {
+    for(let value of [true, false]){
+      const desc = TestUtils.renderIntoDocument(<ObjectDescription object={value} />);
+      const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+      expect(React.findDOMNode(span).textContent).toBe(value.toString());
+    }
+  });
+
+  it('undefined', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription />);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("undefined");
+  });
+
+  it('null', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={null}/>);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("null");
+  });
+
+  it('date', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={new Date('December 17, 1995 03:24:00')}/>);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("Sun Dec 17 1995 03:24:00 GMT-0800 (PST)");
+  });
+
+  it('array', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={[1,2,3,4,5]}/>);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("Array[5]");
+  });
+
+  it('object: empty object', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={{}}/>);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("Object");
+  });
+
+  it('object', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={{'k': 'v'}}/>);
+    const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+
+    expect(React.findDOMNode(span).textContent).toBe("Object");
+  });
+
+  it('function', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={function(){}}/>);
+    const spans = TestUtils.scryRenderedDOMComponentsWithTag(desc, 'span');
+
+    expect(spans.length).toBe(3);
+  });
+
+  it('function: id function', () => {
+    const desc = TestUtils.renderIntoDocument(<ObjectDescription object={function id(a){return a;}}/>);
+    const spans = TestUtils.scryRenderedDOMComponentsWithTag(desc, 'span');
+
+    expect(spans.length).toBe(3);
+    expect(React.findDOMNode(spans[0]).textContent).toBe("function\xa0id()");
+  });
+
+  // Need phantomjs 2
+  // it('symbol', () => {
+  //   const desc = TestUtils.renderIntoDocument(<ObjectDescription object={Symbol("foo")}/>);
+  //   const span = TestUtils.findRenderedDOMComponentWithTag(desc, 'span');
+  //
+  //   expect(React.findDOMNode(span).textContent).toBe("Symbol()");
+  // });
 
 });
