c9832c4ae7cf74a883c916e17e77bd094f00b19f
Cody Reichert <cody@reichertbrothers.com>
2016-12-15 21:40:45 -0600

    Export ObjectLabel and ObjectRootLabel for consumption
    We export these now becuase the defaultNodeRenderer uses these in it's
    implentation and library users should be able to match that (eg, if you
    just want to make minor modifications to the output).

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ export ObjectInspector from './object-inspector/ObjectInspector';
 export TableInspector from './table-inspector/TableInspector';
 export DOMInspector from './dom-inspector/DOMInspector';
 
+export ObjectLabel from './object-inspector/ObjectLabel'
+export ObjectRootLabel from './object-inspector/ObjectRootLabel'
+
 // NOTE: ObjectValue and ObjectPreview can be used as building blocks, but currently their styles are not complete
 // export ObjectValue from './object/ObjectValue'
 // export ObjectPreview from './object/ObjectPreview'
