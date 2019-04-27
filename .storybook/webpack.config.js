9256b816c23d7700ea81190933959d356ce5842d
Norbert de Langen <ndelangen@me.com>
2019-04-27 19:48:40 +0200

    FIX storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,13 @@
 var path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        loaders: ['react-hot-loader', 'babel-loader'],
-        include: [path.join(__dirname, 'src')],
-        // include: [path.join(__dirname, 'src'), path.join(__dirname, 'example')]
-      },
-    ],
-  },
+module.exports = async ({ config }) => {
+  console.log(config);
+  config.module.rules.push({
+    test: /\.(js|jsx)$/,
+    loaders: ['react-hot-loader', 'babel-loader'],
+    include: [path.join(__dirname, 'src')],
+    // include: [path.join(__dirname, 'src'), path.join(__dirname, 'example')]
+  });
+
+  return config;
 };
