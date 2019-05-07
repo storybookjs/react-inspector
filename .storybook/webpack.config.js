ee8c9823ba438a2e3aef6aea9f891ea44bb80d34
Norbert de Langen <ndelangen@me.com>
2019-05-07 13:13:24 +0200

    FIX issues

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,10 @@
 var path = require('path');
 
 module.exports = async ({ config }) => {
-  console.log(config);
   config.module.rules.push({
     test: /\.(js|jsx)$/,
     loaders: ['react-hot-loader', 'babel-loader'],
     include: [path.join(__dirname, 'src')],
-    // include: [path.join(__dirname, 'src'), path.join(__dirname, 'example')]
   });
 
   return config;
