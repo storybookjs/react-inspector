65e96375307e8ce9d72db3beb1274ef7c80debb2
Yeison Daza <jahir045@gmail.com>
2019-09-18 21:26:51 -0500

    add test

--- src/object-inspector/ObjectInspector.spec.js
+++ src/object-inspector/ObjectInspector.spec.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import { render } from 'react-dom'
+import { act } from 'react-dom/test-utils';
 import ObjectInspector from './ObjectInspector';
+
+let container;
 
 describe('ObjectInspector', () => {
   it('should render', () => {
@@ -18,3 +22,33 @@ describe('ObjectInspector', () => {
     expect(tree).toMatchSnapshot();
   });
 });
+
+describe('ObjectInspector Content', () => {
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+  
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  it('should render with Maps with Regex and Maps keys', () => {
+    const data = new Map([
+      [/\S/g, 'Regular Expression key']
+    ]);
+    
+    act(() => {
+      render(<ObjectInspector data={data} />, container);
+    });
+    
+    const button = container.querySelector('div');
+    
+    act(() => {
+      button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    });
+    
+    expect(container.innerHTML).toMatchSnapshot();
+  });
+})
