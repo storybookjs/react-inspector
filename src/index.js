d7abf83d84fd2071404e56c4756cb830b55321ff
David Samudio <luminaxster@gmail.com>
2021-10-21 12:46:07 -0400

    fixes exports.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 export { chromeLight, chromeDark } from './styles/themes';
 
 export ObjectInspector, {useObjectIterator} from './object-inspector/ObjectInspector';
-export ResizableTableInspector,
-{
-  useResizableTable, resizableTableAcceptor
-} from './table-inspector/ResizableTableInspector';
+export DOMInspector, {useDOMIterator} from './dom-inspector/DOMInspector';
+export ResizableTableInspector, {useResizableTable, resizableTableAcceptor}
+  from './table-inspector/ResizableTableInspector';
 export TableInspector from './table-inspector/TableInspector';
 export { tableAcceptor, useTable } from './table-inspector/Table';
-export DOMInspector from './dom-inspector/DOMInspector';
-
 export ObjectLabel from './object-inspector/ObjectLabel';
 export ObjectPreview from './object-inspector/ObjectPreview';
 export ObjectRootLabel from './object-inspector/ObjectRootLabel';
@@ -18,7 +15,8 @@ export ObjectName from './object/ObjectName';
 
 export { useStyles, themeAcceptor } from './styles';
 
-export {typeComparator, isObject, isArray, isIterable, isFunction, isNode} from './utils/typeUtils';
+export {typeComparator, isObject, isArray, isIterable, isFunction, isNode}
+  from './utils/typeUtils';
 
 // Wrapping the inspectors
 import ObjectInspector from './object-inspector/ObjectInspector';
