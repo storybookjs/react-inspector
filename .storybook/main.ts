cc830c06326cdd91fa6d8c92d3133024c9124642
Norbert de Langen <ndelangen@me.com>
2025-09-02 17:00:59 +0200

    chore: Update Storybook configuration to disable minification for better function name preservation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,4 +4,13 @@ export default {
   framework: {
     name: '@storybook/react-vite',
   },
+
+  viteFinal: async (config) => {
+    // Disable minification to preserve function names
+    if (config.build) {
+      config.build.minify = false;
+    }
+
+    return config;
+  },
 };
